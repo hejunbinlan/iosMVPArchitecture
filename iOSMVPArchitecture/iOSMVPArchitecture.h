a0f83d85858f66faf7413a088c3abf455e7664ff
Eduardo Vianna <vianna.eduardo@hotmail.com>
2015-04-26 20:14:39 -0300

    Initial Commit

---
+++ iOSMVPArchitecture/iOSMVPArchitecture.h
@@ -0,0 +1,19 @@
+//
+//  iOSMVPArchitecture.h
+//  iOSMVPArchitecture
+//
+//  Created by Eduardo Vianna on 26/04/15.
+//  Copyright (c) 2015 rocketscience. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSMVPArchitecture.
+FOUNDATION_EXPORT double iOSMVPArchitectureVersionNumber;
+
+//! Project version string for iOSMVPArchitecture.
+FOUNDATION_EXPORT const unsigned char iOSMVPArchitectureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSMVPArchitecture/PublicHeader.h>
+
+
---
+++ iOSMVPArchitectureTests/iOSMVPArchitectureTests.m
@@ -0,0 +1,40 @@
+//
+//  iOSMVPArchitectureTests.m
+//  iOSMVPArchitectureTests
+//
+//  Created by Eduardo Vianna on 26/04/15.
+//  Copyright (c) 2015 rocketscience. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface iOSMVPArchitectureTests : XCTestCase
+
+@end
+
+@implementation iOSMVPArchitectureTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
